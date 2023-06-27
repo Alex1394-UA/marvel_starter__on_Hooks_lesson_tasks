59c5bb077d9d384f0a7121a01d0a23ee4eccd71d
Alex <ohmadeira@gmail.com>
2023-06-27 10:01:59 +0400

    a problem with focus on element in CharList solved with using useMemo hook

--- src/components/comicsList/ComicsList.js
+++ src/components/comicsList/ComicsList.js
@@ -8,6 +8,21 @@ import useMarvelService from '../../services/MarvelService';
 
 import './comicsList.scss';
 
+const setContent = (process, Component, newItemLoading) => {
+    switch (process) {
+        case 'waiting':
+            return <Spinner/>;
+        case 'loading':
+            return newItemLoading ? <Component/> : <Spinner/>;
+        case 'confirmed':
+            return <Component/>;
+        case 'error':
+            return <ErrorMessage/>;
+        default:
+            throw new Error('Unexpected process state');
+    }
+}
+
 const ComicsList = () => {
 
     const [comicsList, setComicsList] = useState([]);
@@ -15,7 +30,7 @@ const ComicsList = () => {
     const [offset, setOffset] = useState(1535);
     const [comicsEnded, setComicsEnded] = useState(false);
     
-    const {loading, error, getAllComics} = useMarvelService();
+    const {getAllComics, process, setProcess} = useMarvelService();
 
     useEffect(() => {
         onRequest(offset, true);
@@ -25,6 +40,7 @@ const ComicsList = () => {
         initial ? setNewItemLoading(false) : setNewItemLoading(true);
         getAllComics(offset)
             .then(onComicsListLoaded)
+            .then(() => setProcess('confirmed'));
     }
 
 
@@ -62,16 +78,10 @@ const ComicsList = () => {
         )
     }
 
-    const items = renderItems(comicsList);
-
-    const errorMessage = error ? <ErrorMessage/> : null;
-    const spinner = loading && !newItemLoading ? <Spinner/> : null;
 
     return (
             <div className="comics__list">
-                {errorMessage}
-                {spinner}
-                {items}
+                {setContent(process, () => renderItems(comicsList), newItemLoading)}
                 <button 
                     className="button button__main button__long"
                     disabled={newItemLoading} 
